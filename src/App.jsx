aa3d28316b503ba82dc55b89ab33d7d63b17e426
arthur <arthurdesouzareis222@gmail.com>
2024-03-20 11:53:15 -0300

    feat: criar contexto de autenticação

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/AppRoutes';
+import { AuthProvider } from './shared/context/Auth';
 
 export const App = () => {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
     </BrowserRouter>
   );
 };
---
+++ src/shared/context/Auth.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { createContext } from 'react';
+
+export const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  return (
+    <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>
+  );
+};
