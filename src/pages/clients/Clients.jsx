e975f9d8de872bf84edf681c7308a2111ada8f78
arthur <arthurdesouzareis222@gmail.com>
2024-03-21 14:32:45 -0300

    feat: criar pagina de cadastro de clientes

--- src/pages/clients/Clients.jsx
+++ src/pages/clients/Clients.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import { LayoutDashBoard } from '../../shared/layout';
-import { Header } from '../../shared/components';
+import { ButtonSubmit, Header, TextField } from '../../shared/components';
+import * as S from './styles';
 export const Clients = () => {
   return (
     <LayoutDashBoard>
       <Header title="Clientes" />
-      Clients
+      <S.Form>
+        <S.Title>Cadastrar Cliente</S.Title>
+        <S.Box>
+          <TextField
+            label="Nome"
+            placeholder="Insira o nome do cliente"
+            type="text"
+            name="name"
+          />
+          <TextField
+            label="CNPJ"
+            placeholder="Insira o CNPJ"
+            name="cnpj"
+            type="text"
+          />
+          <TextField
+            label="Endereço"
+            placeholder="Insira o endereço da empresa"
+            name="adress"
+            type="text"
+          />
+          <TextField
+            label="Email*"
+            placeholder="email@email.com"
+            name="email"
+            type="text"
+          />
+          <TextField
+            label="Telefone"
+            placeholder="Insira  um número para contato"
+            name="telefone"
+            type="text"
+          />
+        </S.Box>
+        <ButtonSubmit type="submit">Cadastrar</ButtonSubmit>
+      </S.Form>
     </LayoutDashBoard>
   );
 };
---
+++ src/pages/clients/styles.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+  padding: 2rem;
+  border-radius: var(--rounded-200);
+  background-color: white;
+`;
+
+export const Box = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1.25rem;
+`;
+
+export const Title = styled.h2`
+  font-size: 1.75rem;
+  font-weight: var(--semibold);
+`;
