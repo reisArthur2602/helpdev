882b31089c6b20adb40d9f8160ff6ed42a0dc2c3
arthur <arthurdesouzareis222@gmail.com>
2024-03-21 15:57:02 -0300

    style: adicionar gap aos elementos do ButtonSubmit

--- src/shared/components/button-submit/styles.jsx
+++ src/shared/components/button-submit/styles.jsx
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
   max-width: 22.5rem;
   width: 100%;
   padding-block: 1rem;
   background-color: var(--purple);
+  color: var(--light-grey-100);
   font-size: 0.875rem;
   font-weight: var(--semibold);
-  color: var(--light-grey-100);
   border-radius: var(--rounded-200);
 `;
